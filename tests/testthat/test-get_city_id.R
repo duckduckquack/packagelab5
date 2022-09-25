36f1c6b0903d84d25de2ccedb38d622088a49d75
duckduckquack <elogins34@tutanota.com>
2022-09-25 09:05:46 +0200

    Add test for get_city_id

---
+++ tests/testthat/test-get_city_id.R
@@ -0,0 +1,15 @@
+test_that("get_city_id works: ", {
+  expect_equal(get_city_id("ale"), "1440")
+  expect_equal(get_city_id("Ale"), "1440")
+  expect_equal(get_city_id("kiruna"), "2584")
+  expect_equal(get_city_id("KIRUNA"), "2584")
+  expect_equal(get_city_id("stockholm"), "0180")
+})
+
+test_that("get_city_id, wrong input errors up: ", {
+  expect_error(get_city_id(5))
+  expect_error(get_city_id("helsinki"))
+  expect_error(get_city_id("a"))
+  expect_error(get_city_id(list()))
+  expect_error(get_city_id("stock"))
+})
