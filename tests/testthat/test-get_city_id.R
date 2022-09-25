25f51f4a41b2390ea48b2dee7ed73b108947ed91
duckduckquack <elogins34@tutanota.com>
2022-09-25 09:16:53 +0200

    Resolve merge conflicts

--- tests/testthat/test-get_city_id.R
+++ tests/testthat/test-get_city_id.R
@@ -6,6 +6,8 @@ test_that("get_city_id works: ", {
   expect_equal(get_city_id("stockholm"), "0180")
 })
 
+
+
 test_that("get_city_id, wrong input errors up: ", {
   expect_error(get_city_id(5))
   expect_error(get_city_id("helsinki"))
